49c0ca4747621d4e2efbca1b873ddd7a29f935de
ClemHlrdt <clement.helardot@gmail.com>
2017-12-12 20:35:18 +0100

    Two new views added for login and profile and logic behind registration.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
+
+//use sessions for tracking logins
+app.use(session( {
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false
+}));
 
 // mongodb connection
 mongoose.connect("mongodb://localhost:27017/bookworm");
@@ -17,8 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 // view engine setup
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'pug'); //use pug
+app.set('views', __dirname + '/views'); //reference directory for views
 
 // include routes
 var routes = require('./routes/index');
